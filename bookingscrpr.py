58da14d39215db2be37051194743b25a4c43a7b2
icwienerr <66826170+icwienerr@users.noreply.github.com>
2022-06-09 21:24:02 +0200

    Add files via upload

--- bookingscrpr.py
+++ bookingscrpr.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import json
+from engine import ngine
 
 
 nombres=[]
@@ -19,6 +20,10 @@ is_verbose = True
 today = datetime.datetime.now()
 tomorrow = today + datetime.timedelta(1)
 
+REQUEST_HEADER = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36"}
+BOOKING_PREFIX = 'https://www.booking.com'
+ROW_PER_OFFSET = 25
+
 def process_data(people, country, city, datein, dateout, is_detail, limit):
     session = requests.Session()
     #people=2
@@ -28,52 +33,28 @@ def process_data(people, country, city, datein, dateout, is_detail, limit):
     #dateout= datetime.datetime.now() + datetime.timedelta(7)
     offset=0
 
-    REQUEST_HEADER = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36"}
-    BOOKING_PREFIX = 'https://www.booking.com'
-    ROW_PER_OFFSET = 25
+
 
     starting_url = create_url(people, country, city, datein, dateout, offset)
-        # print(starting_url)
+
     print("[~] Url created:" + "\n" + "\t" + starting_url)
     response = session.get(starting_url, headers=REQUEST_HEADER)
 
-    #with open("response.html") as fp:
     soup = BeautifulSoup(response.text, "html.parser")
-    #print(response.text)
+
         
     hotels=soup.find_all("div", class_="a826ba81c4 fe821aea6c fa2f36ad22 afd256fc79 d08f526e0d ed11e24d01 ef9845d4b3 da89aeb942" )
 
 
     for hotel in hotels:
-        try:
-            link=hotel.find("a")["href"]
-        except:
-            link=""
-        try:
-            name=hotel.find("div", class_="fcab3ed991 a23c043802").text.strip()
-        except:
-            name=""
-        try:        
-            image = hotel.find("img", class_="b8b0793b0e")["src"]
-        except:
-            image=""
-        try:
-            price = hotel.find("span", class_="fcab3ed991 bd73d13072").text.strip("€")
-        except:
-            price=""
-        try:
-            score = hotel.find("div", class_="b5cd09854e d10a6220b4").text.strip()
-        except:
-            score=""
-        try:
-            near =  hotel.find("span", class_="acb0d5ead1").text.strip()
-        except:
-            near=""
-        try:
-            address = hotel.find("span", class_="f4bd0794db b4273d69aa").text.strip()
-        except:
-            address = ""
 
+        link = ngine.get_link(hotel)
+        name = ngine.get_name(hotel)
+        image = ngine.get_img(hotel)
+        score = ngine.get_score(hotel)
+        near = ngine.get_near(hotel)
+        address = ngine.get_address(hotel)
+        price = ngine.get_price(hotel)
 
         nombres.append(name)
         links.append(link)
@@ -83,13 +64,13 @@ def process_data(people, country, city, datein, dateout, is_detail, limit):
         cerca.append(near)
         ubicacion.append(address)   
 
-    data = {'Nombre': nombres,
+    data = {'Name': nombres,
         'Link': links,
-        'Imagen': imagenes,
-        'Precio': precio,  
-        'Puntuacion': puntuacion,
-        'Cerca' : cerca,
-        'Ubicacion' : ubicacion
+        'Img': imagenes,
+        'Price': precio,  
+        'Score': puntuacion,
+        'Near' : cerca,
+        'Location' : ubicacion
            
            }   
     df=pd.DataFrame(data)
@@ -117,8 +98,10 @@ def create_url(people, country, city, datein, dateout, offset):
 def retrieve_data(people, country, city, datein, dateout, outdir, is_detail, limit):
 
     result = []
-    if isinstance(datein, str) or isinstance(dateout, str):
+    if isinstance(datein, str):
         datein = datetime.datetime.strptime(datein, "%Y-%m-%d")
+
+    if isinstance(dateout, str):
         dateout = datetime.datetime.strptime(dateout, "%Y-%m-%d")
 
     result = process_data(people, country, city, datein, dateout, is_detail, limit)
@@ -132,9 +115,8 @@ def retrieve_data(people, country, city, datein, dateout, outdir, is_detail, lim
     with open(outdir, 'w', encoding='utf-8') as f:
         parse = json.loads(result)
         json.dump(parse, f, ensure_ascii=False, indent=4)
-        #f.writelines(result)
         f.close()
-    #print(result)
+
     print("[~] Process finished!")
 
 
